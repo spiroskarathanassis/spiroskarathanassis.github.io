a6a2c21a0082649e5984181d221bc3ae39cb1d85
Spiros Karathanassis <48492788+spiroskarathanassis@users.noreply.github.com>
2020-02-12 03:02:52 +0200

    Update menuContent.js

--- js/menuContent.js
+++ js/menuContent.js
@@ -1,13 +1,16 @@
-/* Menu Hoverable Appear */
-$(document).ready(function(){
-	$('#menuButton').hover(function() {
-		$('#menuBoxes').show();
-	}, function() {
-		$('#menuBoxes').hide();
-	});
-	$('#menuBoxes').hover(function() {
-		$('#menuBoxes').show();
-	}, function() {
-		$('#menuBoxes').hide();
-	});
+$('#menuButton').click(function() {
+	document.getElementById('menuBoxes').classList.toggle("show");
 });
+
+window.onclick = function(event) {
+	if(!event.target.matches('.menu-modal')) {
+		var menuElements = document.getElementById('menuBoxes');
+		var i;
+		for (i=0; i<menuElements.length; i++) {
+			var openMenu = menuElements[i];
+			if(openMenu.classList.contains('show')) {
+				openMenu.classList.remove('show');
+			}
+		}
+	}
+}
