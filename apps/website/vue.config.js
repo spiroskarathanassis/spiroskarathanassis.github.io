2db732836f5a12e673089c1a52ae2e4ac30f171f
spiroskarathanassis <skarathanassisdev@gmail.com>
2021-01-16 21:46:54 +0200

    change dist directory

--- apps/website/vue.config.js
+++ apps/website/vue.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "../../dist/website")
+  outputDir: path.resolve(__dirname, "../../dist/website"),
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/dist/website'
+    : '/'
 }
