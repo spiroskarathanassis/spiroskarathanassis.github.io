5e2be927832670e92f72d6a33056ce3a2eda0763
spiroskarathanassis <skarathanassisdev@gmail.com>
2021-01-16 20:21:48 +0200

    add menu details

--- apps/website/src/App.vue
+++ apps/website/src/App.vue
@@ -11,6 +11,15 @@
         <img src="./assets/worldwide.png" alt="location" />
         <a href="https://www.google.com/maps/place/Patras">Patras, Greece</a>
       </div>
+      <details>
+        <summary>Menu</summary>
+        <ul>
+          <li>Bio</li>
+          <li>Contact Me</li>
+          <li>Portfolio</li>
+          <li>Skills</li>
+        </ul>
+      </details>
     </main-page>
   </home-page>
 </template>
@@ -25,6 +34,12 @@ export default {
 <style lang="sass">
   @import url("https://fonts.googleapis.com/css?family=Noto+Sans+JP|Amatic+SC")
 
+  $media-mobile: 400px
+  // $media-tablet: 600px
+
+  $salmon-color: salmon  
+  $toggle-color: rgba(74, 44, 44, 0.9)
+
   *, body, html
     box-sizing: border-box
     padding: 0
@@ -34,7 +49,7 @@ export default {
   
   header
     position: absolute
-    background: linear-gradient(to top, rgba(69, 58, 12, 0.6) 20%, transparent), url('./assets/background.jpeg') left 10% bottom 30%/cover border-box
+    background: linear-gradient(to top, rgba(69, 58, 12, 0.7) 20%, transparent), url('./assets/background.jpeg') left 10% bottom 30%/cover border-box
     top: 0
     width: 100vw
     height: 35%
@@ -44,8 +59,9 @@ export default {
       justify-content: flex-end
       align-items: flex-end
       height: 100%
-      padding: 20px
-      font-family: 'Noto Sans JP'
+      padding: 0rem 1rem
+      font-family: fantasy
+      color: rgba(4, 1, 1, 0.88)
       font-size: 1.4em
       font-weight: bold
     
@@ -56,7 +72,7 @@ export default {
     bottom: 0
     width: 100vw
     height: 65%
-    padding: 2rem 0
+    padding: 2rem
     display: flex
     justify-content: flex-start
     align-items: center
@@ -71,7 +87,7 @@ export default {
       padding: 2rem 0 0.5rem 0
   
     .location-container
-      background-color: rgba(74, 44, 44, 0.9)
+      background-color: $toggle-color
       border-radius: 30px
       width: 10rem
       height: 2rem
@@ -87,4 +103,61 @@ export default {
         font-family: cursive
         vertical-align: top
         line-height: 2rem
+
+    details
+      margin-top: 5rem
+      display: flex
+      justify-content: center
+      align-items: center
+      
+      :focus
+        outline: none
+
+      summary
+        background-color: $salmon-color
+        border: 2px solid $salmon-color
+        color: $toggle-color
+        padding: 0.2rem 10rem
+        border-radius: 20px
+        cursor: pointer
+        box-shadow: 0px 0px 10px 1px rgb(175 68 68 / 90%)
+        font-weight: bold
+
+        &::-webkit-details-marker
+          display: none
+       
+        &:hover
+          background-color: $toggle-color
+          border-color: $toggle-color
+          color: $salmon-color
+      
+      ul
+        margin-top: 1.5rem
+        display: flex
+        justify-content: space-between
+
+      li
+        background: rgb(60, 56, 56)
+        width: 5.5rem
+        text-align: center
+        list-style-type: none
+        border-radius: 4px
+        font-size: 0.8em
+        padding: 0.5rem
+        margin: 2px
+        cursor: pointer
+
+        &:hover
+          background: rgba(49, 46, 46, 0.88)
+        
+      @media screen and (max-width: $media-mobile)
+        summary
+          padding: 0.2rem 6rem
+
+        ul
+          margin-top: 1rem
+          flex-direction: column
+          justify-content: center
+          align-items: center
+    
 </style>
